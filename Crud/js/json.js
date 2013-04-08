e3e7f6dce0e6225550b67d1246d7dd3ae90e32ef
Troy Stevenson <troy3580@fullsail.edu>
2013-04-08 16:49:07 -0700

    crud

---
+++ Crud/js/json.js
@@ -0,0 +1,223 @@
+//JSON Object
+
+var json = {
+	"employee1": {
+		"group":     ["Group", "GSR"],
+		"fName":     ["First Name", "Anthony"],
+		"lName":     ["Last Name", "Peterson"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Anthony@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Always on time!"]
+		},
+		
+	"employee2": {
+		"group":     ["Group", "Housekeeper"],
+		"fName":     ["First Name", "Blanca"],
+		"lName":     ["Last Name", "Valdez"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Blanca@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Friendly"]
+		},
+	
+	"employee3": {
+		"group":     ["Group", "Maintenance"],
+		"fName":     ["First Name", "Kevin"],
+		"lName":     ["Last Name", "Comestock"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Kevin@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Handy"]
+		},
+	
+	"employee4": {
+		"group":     ["Group", "GSR"],
+		"fName":     ["First Name", "Lilli"],
+		"lName":     ["Last Name", "Pad"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Lilli@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Only Works Friday and Saturday"]
+		},
+	
+	"employee5": {
+		"group":     ["Group", "GSR"],
+		"fName":     ["First Name", "Cruz"],
+		"lName":     ["Last Name", "Wooten"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "cruz@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Graveyard Shift too"]
+		},
+	
+	"employee6": {
+		"group":     ["Group", "Housekeeping"],
+		"fName":     ["First Name", "Loura"],
+		"lName":     ["Last Name", "Martin"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Loura@fullsail.edu"],
+		"time":		 ["Time", "Night"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Doesn't have a cell phone"]
+		},
+	
+	"employee7": {
+		"group":     ["Group", "Maintenance"],
+		"fName":     ["First Name", "Kevin"],
+		"lName":     ["Last Name", "Comestock"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Kevin@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Handy"]
+		},
+	
+	"employee8": {
+		"group":     ["Group", "GSR"],
+		"fName":     ["First Name", "Ryan"],
+		"lName":     ["Last Name", "Sully"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Ryan@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "95"],
+		"notes":     ["Notes", "Can work Grave."]
+		},
+	
+	"employee9": {
+		"group":     ["Group", "GSR"],
+		"fName":     ["First Name", "Mark"],
+		"lName":     ["Last Name", "Daily"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Mark@fullsail.edu"],
+		"time":		 ["Time", "Night"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Kan't work Thursdays."]
+		},
+	
+	"employee10": {
+		"group":     ["Group", "Housekeeping"],
+		"fName":     ["First Name", "Janna"],
+		"lName":     ["Last Name", "Stevenson"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Janna@fullsail.edu"],
+		"time":		 ["Time", "Night"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Friedly"]
+		},
+	
+	"employee11": {
+		"group":     ["Group", "Maintenance"],
+		"fName":     ["First Name", "Glen"],
+		"lName":     ["Last Name", "Tall"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Glen@fullsail.edu"],
+		"time":		 ["Time", "Night"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Put in his 2 weeks."]
+		},
+	
+	"employee12": {
+		"group":     ["Group", "GSR"],
+		"fName":     ["First Name", "Trevor"],
+		"lName":     ["Last Name", "Green"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Trevor@fullsail.edu"],
+		"time":		 ["Time", "Night"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Has an aniversory coming up."]
+		},
+	
+	"employee13": {
+		"group":     ["Group", "GSR"],
+		"fName":     ["First Name", "Todd"],
+		"lName":     ["Last Name", "Lander"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Todd@fullsail.edu"],
+		"time":		 ["Time", "Night"],
+		"reliable":  ["Reliable", "80"],
+		"notes":     ["Notes", "Works to jobs."]
+		},
+	
+	"employee14": {
+		"group":     ["Group", "Housekeeping"],
+		"fName":     ["First Name", "Little"],
+		"lName":     ["Last Name", "Blanca"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "LB@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Always funny"]
+		},
+	
+	"employee15": {
+		"group":     ["Group", "Maintenance"],
+		"fName":     ["First Name", "Edgar"],
+		"lName":     ["Last Name", "Golzolis"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Edgar@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Lazy"]
+		},
+	
+	"employee16": {
+		"group":     ["Group", "GSR"],
+		"fName":     ["First Name", "Cheryl"],
+		"lName":     ["Last Name", "Johnson"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Johnson@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Drives a red car."]
+		},
+	
+	"employee17": {
+		"group":     ["Group", "GSR"],
+		"fName":     ["First Name", "Ed"],
+		"lName":     ["Last Name", "Taylor"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Ed@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Graveyard Shift too"]
+		},
+	
+	"employee18": {
+		"group":     ["Group", "Housekeeping"],
+		"fName":     ["First Name", "Gloria"],
+		"lName":     ["Last Name", "Martinez"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Gloria@fullsail.edu"],
+		"time":		 ["Time", "Day"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Lives with Blanca"]
+		},
+	
+	"employee19": {
+		"group":     ["Group", "GSR"],
+		"fName":     ["First Name", "Karen"],
+		"lName":     ["Last Name", "Wood"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Karen@fullsail.edu"],
+		"time":		 ["Time", "Night"],
+		"reliable":  ["Reliable", "65"],
+		"notes":     ["Notes", "Always Sick"]
+		},
+	
+	"employee20": {
+		"group":     ["Group", "Housekeeping"],
+		"fName":     ["First Name", "Patty"],
+		"lName":     ["Last Name", "Sue"],
+		"pNumber":   ["Phone Number", "(775)555-5555"],
+		"email":     ["Email", "Patty@fullsail.edu"],
+		"time":		 ["Time", "Night"],
+		"reliable":  ["Reliable", "100"],
+		"notes":     ["Notes", "Lives CLose"]
+		}
+	};
